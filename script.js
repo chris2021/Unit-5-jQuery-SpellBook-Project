25e5bafb390d23806ec8756437f13c3f34295afc
chris2021 <44203270+chris2021@users.noreply.github.com>
2019-01-15 10:24:39 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$(".hw").click(function(){
+   $(".s1").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("#jordan").click(function(){
+   $("#sp2").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("#netflix").click(function(){
+    $(".thirdSpell").toggle();
+});
+
+
+
